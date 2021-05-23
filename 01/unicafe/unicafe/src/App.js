f6c93490a607ef286e4c0e736888f3d089854298
Vesa Haaparanta <vesa.haaparanta@tuni.fi>
2021-05-23 17:58:49 +0300

    step6

--- 01/unicafe/unicafe/src/App.js
+++ 01/unicafe/unicafe/src/App.js
@@ -31,6 +31,48 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
+const StatisticsWithTable = ({good, neutral, bad}) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+         <h1>Statistics with table</h1>
+         <p1>No feedback given</p1>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Statistics with table</h1>
+      <tr>
+        <td>Good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>Neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>All</td>
+        <td>{good+neutral+bad}</td>
+      </tr>
+      <tr>
+        <td>Average</td>
+        <td>{(good-bad)/(good+neutral+bad)}</td>
+      </tr>
+      <tr>
+        <td>Positive</td>
+        <td>{(good)/(good+neutral+bad)}</td>
+      </tr>
+    </div>
+  )
+}
+
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
@@ -62,6 +104,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
       <Statistics good={good} neutral={neutral} bad={bad}/>
+      <StatisticsWithTable good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
