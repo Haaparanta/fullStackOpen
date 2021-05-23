538bd3ad1e3f403ff9babc3d10d86e7d2d44e89d
Vesa Haaparanta <vesa.haaparanta@tuni.fi>
2021-05-23 17:33:13 +0300

    step3

--- 01/unicafe/unicafe/src/App.js
+++ 01/unicafe/unicafe/src/App.js
@@ -1,23 +1,9 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
+  
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-          good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-          neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-          bad
-      </button>
       <h1>Statistics</h1>
       <p1>Good {good} <br/></p1>
       <p1>Neutral {neutral} <br/></p1>
@@ -25,6 +11,41 @@ const App = () => {
       <p1>All {good+neutral+bad} <br/></p1>
       <p1>Average {(good-bad)/(good+neutral+bad)} <br/></p1>
       <p1>Positive {(good)/(good+neutral+bad)}</p1>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => handleGoodClick()}>
+          good
+      </button>
+      <button onClick={() => handleNeutralClick()}>
+          neutral
+      </button>
+      <button onClick={() => handleBadClick()}>
+          bad
+      </button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
