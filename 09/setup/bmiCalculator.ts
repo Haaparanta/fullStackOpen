4993c1e35fcdf2331e600f1913fa40f6b08f252c
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-04-02 18:17:38 +0300

    9.5

--- 09/setup/bmiCalculator.ts
+++ 09/setup/bmiCalculator.ts
@@ -16,6 +16,4 @@ const calculateBmi = (height: number, weight: number): string => {
     return result;
 }
 
-const a: number = Number(process.argv[2]);
-const b: number = Number(process.argv[3]);
-console.log(calculateBmi(a, b));
+export default calculateBmi
