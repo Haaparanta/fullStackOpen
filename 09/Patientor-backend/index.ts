fb42f9b68573bac48ef4df32762d6b778a1ba0e0
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-04-02 22:18:27 +0300

    9.10

--- 09/Patientor-backend/index.ts
+++ 09/Patientor-backend/index.ts
@@ -1,14 +1,30 @@
-
-
 import express from "express";
+import DData from "./data/diagnoses.json";
+
+
+interface Diagnose {
+    code: string;
+    name: string;
+    latin?: string; // optional property
+}
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    res.send(DData);
+});
 
 
 const app = express();
+
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use('/api/diagnoses', router);
 
 
 const PORT = 3000;
